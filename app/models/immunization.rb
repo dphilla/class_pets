69cc3ffa4fb20caf89fbd6b5e026454ab91206b4
Daniel Phillips <danieledward.phillips@gmail.com>
2017-07-09 19:39:50 -0600

    finalized seeding

--- app/models/immunization.rb
+++ app/models/immunization.rb
@@ -1,2 +1,4 @@
 class Immunization < ApplicationRecord
+  has_many :species_immunization
+  has_many :species, through: :species_immunization
 end
--- app/models/species.rb
+++ app/models/species.rb
@@ -1,3 +1,5 @@
 class Species < ApplicationRecord
   has_many :pets
+  has_many :species_immunization
+  has_many :immunizations, through: :species_immunization
 end
